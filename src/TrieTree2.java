7e4d8ad014ceb0cad9a7273c1587447fc4108740
sirius <xianwangyue123@163.com>
2019-11-14 13:56:37 +0800

    新增isStartWith方法，用来判断最短匹配

--- src/TrieTree2.java
+++ src/TrieTree2.java
@@ -7,7 +7,7 @@ public class TrieTree2 {
 
     public void add(String word) {
         Node node = node_all;
-        for (int i=0; i<word.length(); i++) {
+        for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
 
             // 如果是新的字，就直接put
@@ -15,23 +15,53 @@ public class TrieTree2 {
             if (!node.content.containsKey(c)) {
                 node.content.put(c, tempNode);
             }
-             node = node.content.get(c);
+            node = node.content.get(c);
 //            node = tempNode;
         }
         node.isEnd = true;
     }
 
-    public static void main(String[] args) {
-        TrieTree2 ins = new TrieTree2();
-        String[] st = new String[]{"af", "bf", "be", "cq", "b",  "w"};
-        for(String word : st) {
-            ins.add(word);
+    /**
+     * 输入的word是否有以字典开头的
+     * 这个是“最短”匹配，也就是一有匹配的词就return了
+     * @param word
+     * @return
+     */
+    public boolean isStartWith(String word) {
+        Node node_all_temp = node_all;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if (node_all_temp.content.containsKey(c)) {
+                // 如果输入词语结束，必须isEnd
+                if ( node_all_temp.content.get(c).isEnd) {
+                    return true;
+                }
+
+                node_all_temp = node_all_temp.content.get(c);
+            }
+
         }
-        System.out.println("===");
+        return false;
     }
+
 
     private static class Node {
         boolean isEnd = false;
         Map<Character, Node> content = new HashMap<>();
     }
+
+    public static void main(String[] args) {
+        TrieTree2 ins = new TrieTree2();
+        String[] st = new String[]{"about", "boyfriend", "bee", "select", "buy", "weak"};
+        for (String word : st) {
+            ins.add(word);
+        }
+        System.out.println(ins.isStartWith("weak123"));
+
+        System.out.println("===");
+    }
+
+
 }
